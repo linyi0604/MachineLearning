21a4069511436e3e0a487224c8461af451fd69ec
linyi0604 <lin_yi_0604@foxmail.com>
2018-05-09 19:13:54 +0800

    2018050901

--- 06_kaggle_competition/02_IMDB电影评分/model.py
+++ 06_kaggle_competition/02_IMDB电影评分/model.py
@@ -2,9 +2,14 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import re
 from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+from sklearn.grid_search import GridSearchCV
 
-import nltk
-nltk.download("stopwords")
+
+# import nltk
+# nltk.download("stopwords")
 
 
 # 从本地读取文件
@@ -30,10 +35,50 @@ def review_to_text(review, remove_stopwords):
     return words
 
 # 分别对训练数据和测试数据进行内容的处理
-x_train = []
+x_train, x_test = [], []
 for review in train["review"]:
     x_train.append(" ".join(review_to_text(review, True)))
 
+
+for review in test["review"]:
+    x_test.append(" ".join(review_to_text(review, True)))
+
 y_train = train["sentiment"]
 
-print(x_train, y_train)
+
+# 利用pipeline搭建两个朴素贝耶斯分类期,分别使用CountVectorizer 和 TfidVectorizer
+pip_count = Pipeline([
+    ("count_vec", CountVectorizer(analyzer="word")),
+    ("mnb", MultinomialNB())
+])
+params_count = {
+    "count_vec__binary": [True, False],
+    "count_vec__ngram_range": [(1, 1), (1, 2)],
+    "mnb__alpha": [0.1, 1.0, 10.1]
+}
+
+pip_tfid = Pipeline([
+    ("tfid_vec", TfidfVectorizer(analyzer="word")),
+    ("mnb", MultinomialNB())
+])
+params_tfid = {
+    "tfid_vec__binary": [True, False],
+    "tfid_vec__ngram_range": [(1, 1), (1, 2)],
+    "mnb__alpha": [0.1, 1.0, 10.1]
+}
+
+# 使用4折交叉验证对CountVectorizer的贝耶斯分类期进行超参数搜索
+gs_count = GridSearchCV(pip_count, params_count, cv=4, n_jobs=5, verbose=1)
+gs_count.fit(x_train, y_train)  # 学习
+# 输出交叉验证中最高的得分和超参数组合
+print(gs_count.best_score_)
+print(gs_count.best_params_)
+
+
+
+
+
+
+
+
+
